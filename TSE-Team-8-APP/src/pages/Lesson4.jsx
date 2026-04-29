6e6eed48cbdc9a4df41277b9e74bfba32f9e795c
QuinnTheMyth <q.myth005@gmail.com>
2026-04-29 15:09:33 +0100

    Created majority of lesson 4

--- TSE-Team-8-APP/src/pages/Lesson4.jsx
+++ TSE-Team-8-APP/src/pages/Lesson4.jsx
@@ -37,10 +37,38 @@ export default function Lesson4(){
 
             {activeOverlay && (
                 <div className="overlay" onClick={() => !showOverlay && setActiveOverlay(null)}>
-                    {activeOverlay}
+                    {activeOverlay === "robot" && (
+                        <p>
+                            The university creates cool robots that can assist in agriculture.
+                            Their robots can identify harvest ready crops, identify disease and
+                            effectively analyse plant traits. You could work on one yourself!<br></br>
+                            -- Tap to hide this message --
+                        </p>
+                    )}
+
+                    {activeOverlay === "workshop" && (
+                        <p>
+                            The refurbished engineering workshops
+                            consist of teaching spaces, a large engineering space split into bays
+                            for dedicated project work and collaborative research, 
+                            and a separate robotics lab. The university provides a large open-plan 
+                            space for scaling, testing and trialling robotics.<br></br>
+                            -- Tap to hide this message --
+                        </p>
+                    )}
+
+                    {activeOverlay === "fact" && (
+                        <p>
+                            Robots can help in hospitals, fix satellites in outer space,
+                            help in agriculture and restore coral reefs in the ocean!<br></br>
+                            -- Tap to hide this message
+                        </p>
+                    )}
                 </div>
             )}
 
+            <button id="robotBtn" onClick={() => setActiveOverlay("robot")}></button>
+            <button id="workshopBtn" onClick={() => setActiveOverlay("workshop")} ></button>
             <button id="funFactBtn" onClick={() => setActiveOverlay("fact")}>Fun Fact</button>
             <button id="homeBtn" onClick={() => navigate("/")}>Home</button>
         </div>
